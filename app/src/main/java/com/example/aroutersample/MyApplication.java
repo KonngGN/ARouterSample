5ccf665412215da89ecc76614133d02be6d2dbc0
jiangnankong <jiangnankong@ecarx.com.cn>
2019-03-07 11:30:00 +0800

    feat:实践(1):简单跳转到同模块下的其他页面

---
+++ app/src/main/java/com/example/aroutersample/MyApplication.java
@@ -0,0 +1,25 @@
+package com.example.aroutersample;
+
+import android.app.Application;
+
+import com.alibaba.android.arouter.launcher.ARouter;
+
+/**
+ * @author : jiangnankong
+ * @date : 3/6/19 2:40 PM
+ * Description :
+ */
+
+public class MyApplication extends Application {
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        if (BuildConfig.DEBUG) {
+            ARouter.openDebug();
+            ARouter.openLog();
+        }
+        ARouter.init(this);
+    }
+}
